847061652e0bb1dfa8c3154a95e62ec439937995
conselio <contact@conselio.com>
2023-03-14 10:27:38 +0100

    add unitcode

--- src/Payment/UnitCode.php
+++ src/Payment/UnitCode.php
@@ -10,18 +10,37 @@ class UnitCode
     const ARE = 'ARE';
     const HECTARE = 'HAR';
     const OUTLIT = 11;
+    const RATE = 'A9';
     const SQUARE_METRE = 'MTK';
     const SQUARE_KILOMETRE = 'KMK';
     const SQUARE_FOOT = 'FTK';
     const SQUARE_YARD = 'YDK';
     const SQUARE_MILE = 'MIK';
+    const CUBIC_METER = 'MTQ';
+    const CENTIMETRE = 'CMT';
+    const KILOMETRE = 'KMT';
+    const KILOTONNE = 'KTN';
+    const GRAM = 'GRM';
+    const KILOGRAM = 'KGM';
+    const LENGHT = 'LN';
+
+    const BOX = 'XBX';
+    const BUCKET = 'XBJ';
+    const BAG = 'XBG';
+    const CARD = 'CG';
+    const ROLL = 'XRO';
     const RATION = 13;
     const LITRE = 'LTR';
+    const MILILITRE = 'MLT';
+    const METRE = 'MTR';
+    const MILLIMETRE = 'MMT';
     const SHOT = 14;
     const SECOND = 'SEC';
     const MINUTE = 'MIN';
     const HOUR = 'HUR';
     const DAY = 'DAY';
+    const WEEK = 'WEE';
     const MONTH = 'MON';
     const YEAR = 'ANN';
+    const LINEAR_METER = 'LM';
 }
